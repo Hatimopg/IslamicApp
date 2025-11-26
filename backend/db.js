f8bd82f1a01859ac3aa00982abe9d1310b6d2b15
Hatimopg <hatimben033@gmail.com>
2025-11-26 14:50:34 +0100

    fix db.js et index pr railway

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import mysql from "mysql2/promise";
 
 export const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306,
-    ssl: {
-      rejectUnauthorized: true
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }
 });
