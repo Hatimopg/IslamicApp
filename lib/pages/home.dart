6470146ae5014f8740a982c071121c1b83df3a99
Hatimopg <hatimben033@gmail.com>
2025-12-26 14:45:56 +0100

    apk v1.8.3 notif adhan

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -107,7 +107,7 @@ class _HomePageState extends State<HomePage> {
         await CityStorage.save(city);
       }
     } catch (_) {
-      setState(() => selectedCity = "Brussels");
+      setState(() => selectedCity = "Ronse");
     }
 
     fetchPrayerTimes();
